4bd1ffb730c7ca063998fae5e6ab1fa1639660bd
James Kirwa <example@email.com>
2019-10-03 00:18:05 +0300

    create a width field

--- photos/models.py
+++ photos/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class photo(models.Model):
     title = models.CharField(max_length=200 )
+    width = models.IntegerField(default=0)
     image = models.ImageField()
     timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
                                      
