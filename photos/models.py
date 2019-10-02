1c0e7d629db4bdc541f8e05b56e471cffb9ee68d
James Kirwa <example@email.com>
2019-10-03 00:47:22 +0300

    make migrations

--- photos/models.py
+++ photos/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from __future__ import unicode_literals
+from django.db import models
 # Create your models here.
 class photo(models.Model):
     title = models.CharField(max_length=200 )
