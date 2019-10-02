647450b06bb1619f483d266166e81ae150f51539
James Kirwa <example@email.com>
2019-10-03 00:12:57 +0300

    create a timestamp

--- photos/models.py
+++ photos/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class photo(models.Model):
     title = models.CharField(max_length=200 )
+    image =
+    timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
+                                     
       
