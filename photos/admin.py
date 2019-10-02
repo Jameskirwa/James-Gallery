cdcc57861231457770db9d6599ffba4163be8f7f
James Kirwa <example@email.com>
2019-10-03 00:40:15 +0300

    create a list to display

--- photos/admin.py
+++ photos/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import photo
 
 # Register your models here.
-class photoAdmin(admin.ModelAdmin)
+class photoAdmin(admin.ModelAdmin):
+    list_display = ["title", "timestamp"]
+    
