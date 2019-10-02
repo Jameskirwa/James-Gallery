fa0411f8c89e3595df5fd3049043c3a871b69822
James Kirwa <example@email.com>
2019-10-03 00:19:53 +0300

    create height field

--- photos/models.py
+++ photos/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class photo(models.Model):
     title = models.CharField(max_length=200 )
     width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
     image = models.ImageField()
     timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
                                      
