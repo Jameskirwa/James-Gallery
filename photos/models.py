e918f5936ffb8a1fab784379d42b4afcfae3ed32
James Kirwa <example@email.com>
2019-10-03 00:24:19 +0300

    add dimentions to image

--- photos/models.py
+++ photos/models.py
@@ -5,7 +5,7 @@ class photo(models.Model):
     title = models.CharField(max_length=200 )
     width = models.IntegerField(default=0)
     height = models.IntegerField(default=0)
-    image = models.ImageField()
+    image = models.ImageField(null=False, blank=False, width_field="width", height_field="height")
     timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
                                      
       
