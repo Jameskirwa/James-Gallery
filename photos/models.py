64211389616cd98c4272f1602193d403502039bd
James Kirwa <example@email.com>
2019-10-03 00:31:15 +0300

    create a classs meta

--- photos/models.py
+++ photos/models.py
@@ -7,5 +7,10 @@ class photo(models.Model):
     height = models.IntegerField(default=0)
     image = models.ImageField(null=False, blank=False, width_field="width", height_field="height")
     timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
+    
+    def __unicode__(self):
+        return self.title
+    class Meta:
+        ordering = ["-timestamp"]
                                      
       
