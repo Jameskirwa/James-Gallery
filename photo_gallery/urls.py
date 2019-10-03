6035f0810060c0ddd7f31e895ecf223b52839bb6
James Kirwa <example@email.com>
2019-10-03 21:28:33 +0300

    import photo_list

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 import os
+from photos.views import photo_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
