dfb749ca6d2ade9d7799150a5468d46aef2323e9
James Kirwa <example@email.com>
2019-10-03 21:22:30 +0300

    create templates folder

--- photos/views.py
+++ photos/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 from .models import photo
 
 # Create your views here.
+def photo_list(request):
+    queryset = photo.objects.all()
+    context = {
+        "photos": queryset,
+    }
+    return render(request, "templates",context)
